ed62a09a8f3884c958b17cab799b4db5a62d8126
Dhaval Shah <d.shah@tum.de>
2020-07-17 12:18:31 +0200

    Finished infer

--- video_infer.py
+++ video_infer.py
@@ -7,9 +7,25 @@ import os
 
 from models.res2net import res2net50
 
+gesture_ids = {}
+
+
+def create_gesture_ids():
+    with open("gesture_ids.txt", "r") as f:
+        gestures = f.readlines()
+    global gesture_ids
+
+    for i in range(0, 35):
+        line = gestures[i]
+        gesture_num = line.split(" = ")[0]
+        gesture_label = line.split(" = ")[-1]
+
+        gesture_ids[gesture_num] = gesture_label
+
 
 def vid_2_imgs(vid_path):
     vid_in = cv.VideoCapture(vid_path)
+    fps = vid_in.get(cv.CAP_PROP_FPS)
     success, img = vid_in.read()
     imgs = []
 
@@ -22,16 +38,33 @@ def vid_2_imgs(vid_path):
         success, image = vid_in.read()
 
     vid_in.release()
+    img_shape = tuple(img[0][0].shape)
+    return imgs, fps, img_shape
 
-    return imgs
 
+def create_vid(gestures, vid_out_name, fps, size):
+    fourcc = cv.VideoWriter_fourcc(*'mp4v')
+    vid_out = cv.VideoWriter(vid_out_name, fourcc, fps, size)
 
-def create_vid(gestures, vid_out_name):
-    pass
+    font = cv.FONT_HERSHEY_SIMPLEX
+    x = size[0] - 40
+    y = size[1] - 40
+    pt = (x, y)
+    scale = 50
+    global gesture_ids
+
+    for i in gestures:
+        curr_gesture = gesture_ids[(str(i + 1))]
+
+        img = np.zeros(size)
+        img = cv.putText(img, curr_gesture, pt, font, scale, (255, 255, 255))
+        vid_out.write(img)
+
+    vid_out.release()
 
 
 def vid_classification(model, vid_path):
-    frames = vid_2_imgs(vid_path)
+    frames, fps, img_shape = vid_2_imgs(vid_path)
 
     model.eval()
     gestures = []
@@ -44,7 +77,7 @@ def vid_classification(model, vid_path):
         _, pred = torch.max(outputs, 1)
         gestures.append(pred)
 
-    return gestures
+    return gestures, fps, img_shape
 
 
 def vid_infer():
@@ -58,9 +91,9 @@ def vid_infer():
     model.cuda()
     model.load_state_dict(torch.load(saved_model_path))
 
-    gestures = vid_classification(model, vid_in_path)
+    gestures, fps, img_shape = vid_classification(model, vid_in_path)
 
-    create_vid(gestures, vid_out_name)
+    create_vid(gestures, vid_out_name, fps, img_shape)
 
 
 if __name__ == "__main__":
