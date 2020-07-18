42bd7a3d03e5f0e83dc8090db9969d90a03cc4ef
Dhaval Shah <d.shah@tum.de>
2020-07-18 19:55:30 +0200

    Fixed inferring pictures into video

--- video_infer.py
+++ video_infer.py
@@ -3,109 +3,131 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pathlib
-import os
-import matplotlib.pyplot as plt
+from subprocess import Popen, PIPE
 
 from models.res2net import res2net50
-
-gesture_ids = {}
-
-
-def create_gesture_ids():
-    with open("gesture_ids.txt", "r") as f:
-        gestures = f.readlines()
-    global gesture_ids
-
-    for i in range(0, 35):
-        line = gestures[i]
-        gesture_num = line.split(" = ")[0]
-        gesture_label = line.split(" = ")[-1]
-
-        gesture_ids[gesture_num] = gesture_label
+from models.effnet import EfficientNet
 
 
-def vid_2_imgs(vid_path):
-    vid_in = cv.VideoCapture(vid_path)
-    fps = vid_in.get(cv.CAP_PROP_FPS)
-    width = int(vid_in.get(cv.CAP_PROP_FRAME_WIDTH))
-    height = int(vid_in.get(cv.CAP_PROP_FRAME_HEIGHT))
-    img_shape = (width, height)
-    success, img = vid_in.read()
-    imgs = []
+class VidInfer:
+    def __init__(self, vid_in_path, vid_out_path, model_path, font_scale=2, font_thickness=2):
+        self.vid_in_path = vid_in_path
+        self.vid_out_path = vid_out_path
 
-    while success:
-        rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        rgb_img = rgb_img.transpose((2, 0, 1))
+        if "resnet" in model_path:
+            self.model = nn.DataParallel(res2net50(num_classes=35))
+        elif "efficient" in model_path or "eff" in model_path:
+            self.model = nn.DataParallel(EfficientNet.from_name("efficientnet-b0", num_classes=35))
 
-        imgs.append(rgb_img)
+        self.model.load_state_dict(torch.load(model_path))
+        self.gesture_ids = {}
+        self.create_gesture_ids()
 
-        success, image = vid_in.read()
+        self.frames = []
+        self.gestures = []
+        self.fps = 0
+        self.size = ()
+        self.scale = font_scale
+        self.thickness = font_thickness
 
-    vid_in.release()
-    return imgs, fps, img_shape
+    def do_vid_infer(self):
+        self.vid_2_imgs()
+        self.vid_classification()
+        self.create_vid()
 
+    def create_gesture_ids(self):
+        """
+        Create dict of gestures from gesture_ids.txt
+        """
+        with open("gesture_ids.txt", "r") as f:
+            gestures = f.readlines()
 
-def create_vid(gestures, vid_out_name, fps, size):
-    fourcc = cv.VideoWriter_fourcc(*'mjpg')
-    vid_out = cv.VideoWriter(vid_out_name, fourcc, 1, size)
+        for i in range(0, 35):
+            line = gestures[i]
+            gesture_num = line.split(" = ")[0].strip()
+            gesture_label = line.split(" = ")[-1].strip()
 
+            self.gesture_ids[gesture_num] = gesture_label
 
-    font = cv.FONT_HERSHEY_SIMPLEX
-    scale = 5
-    thickness = 20
+    def vid_2_imgs(self):
+        """
+        Extract each frame of input video and store in self.frames of object
+        """
+        vid_in = cv.VideoCapture(self.vid_in_path)
+        self.fps = vid_in.get(cv.CAP_PROP_FPS)
+        width = int(vid_in.get(cv.CAP_PROP_FRAME_WIDTH))
+        height = int(vid_in.get(cv.CAP_PROP_FRAME_HEIGHT))
+        self.size = (height, width)
 
-    global gesture_ids
+        success, img = vid_in.read()
 
-    for i in gestures:
-        img = np.zeros(size, dtype=np.uint8)
-        
-        curr_gesture = gesture_ids[str(i + 1)].strip()
-        textsize = cv.getTextSize(curr_gesture, font, scale, thickness)[0]
-        textX = (img.shape[1] - textsize[0]) // 2
-        textY = (img.shape[0] + textsize[1]) // 2
+        while success:
+            # rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            rgb_img = img
+            rgb_img = rgb_img.transpose((2, 0, 1))
 
-        # add text centered on image
-        img = cv.putText(img, curr_gesture, (textX, textY), font, scale, (255, 255, 255), thickness).astype(np.uint8)
-#         plt.imshow(img)
-#         plt.show()
-        
-        vid_out.write(img)
+            self.frames.append(rgb_img)
 
-#     vid_out.release()
+            success, img = vid_in.read()
 
+        vid_in.release()
 
-def vid_classification(model, vid_path):
-    frames, fps, img_shape = vid_2_imgs(vid_path)
+    def vid_classification(self):
+        """
+        Use model to classify each image in self.frames and save predicted gestures in self.gestures
+        """
+        self.model.eval()
 
-    model.eval()
-    gestures = []
-    for i in frames:
-        i = torch.from_numpy(i).cuda().to(torch.float)
-        i = i.unsqueeze(0)
+        for i in self.frames:
+            i = torch.from_numpy(i).to(torch.float)
+            i = i.unsqueeze(0)
 
-        outputs = model(i)
+            outputs = self.model(i)
 
-        _, pred = torch.max(outputs, 1)
-        gestures.append(int(pred.detach().cpu().numpy()))
+            val, pred = torch.max(outputs, 1)
+            self.gestures.append([int(val.detach().cpu().numpy()), int(pred.detach().cpu().numpy())])
 
-    return gestures, fps, img_shape
+    def create_vid(self):
+        """
+        Find label of predicted gesture, create image with label text, and create video
+        """
+        font = cv.FONT_HERSHEY_SIMPLEX
+        num = 0
 
+        print("CREATING VIDEO")
+        # Creating images for each frame
+        for j, (val, i) in enumerate(self.gestures):
+            img = np.zeros(self.size)
+            curr_gesture = self.gesture_ids[str(i + 1)].strip()
+            curr_gesture = str(val) + " " + curr_gesture
 
-def vid_infer():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1,0"
+            textsize = cv.getTextSize(curr_gesture, font, self.scale, self.thickness)[0]
+            text_x = (img.shape[1] - textsize[0]) // 2
+            text_y = (img.shape[0] + textsize[1]) // 2
 
-    vid_in_path = "/home/dshah/2dgesturenet/vid_in.mpg"
-    vid_out_name = "/home/dshah/2dgesturenet/vid_out.mpg"
+            # add text centered on image
+            img = cv.putText(img, curr_gesture, (text_x, text_y), font, self.scale, (255, 255, 255), self.thickness).astype(np.uint8)
 
-    saved_model_path = "/home/dshah/2dgesturenet/res2net50.pth"
-    model = nn.DataParallel(res2net50(num_classes=35))
-    model.cuda()
-    model.load_state_dict(torch.load(saved_model_path))
+            cv.imwrite("./tmp/img_%03d.png" % num, img)
+            num += 1
 
-    gestures, fps, img_shape = vid_classification(model, vid_in_path)
+        # Using created images to make video with ffmpeg
+        # frame rate 25
+        process = Popen(['ffmpeg', '-y', '-i', './tmp/img_%03d.png', self.vid_out_path], stdout=PIPE, stderr=PIPE)
+        process.communicate()
 
-    create_vid(gestures, vid_out_name, fps, img_shape)
+        path = pathlib.Path("./tmp")
+        img_paths = path.glob("img*.png")
+        for file in img_paths:
+            file.unlink()
 
 
 if __name__ == "__main__":
-    vid_infer()
+    vid_in = "/home/dhaval/2d_gesture_classification/vid_in_1.mpg"
+    vid_out = "vid_out_1.mp4"
+    saved_model_path = "/home/dhaval/2d_gesture_classification/efficientnet-b0.pth"
+
+    vid_infer = VidInfer(vid_in, vid_out, saved_model_path)
+    vid_infer.do_vid_infer()
+
+
